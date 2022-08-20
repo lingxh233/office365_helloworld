369422d9533026c779f51de5591b7741821309dd
lingxh233 <lingxh2767@gmail.com>
2022-08-20 16:19:02 +0800

    Update office_test.cpp

--- office_test.cpp
+++ office_test.cpp
@@ -1,7 +1,4 @@
-﻿// unicode test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main()
@@ -9,7 +6,8 @@ int main()
 	cout << "hello world "<< endl;
 	//输出当前时间
 	cout << "当前时间：" << __DATE__ << " " << __TIME__ << endl;
-	//输出unix时间戳
-	cout << "unix时间戳：" << __TIME__ << endl;
+	//两数之和
+	int a = 10, b = 20;
+	cout << "a + b = " << a + b << endl;
 }
 
